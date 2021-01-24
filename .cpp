380bd83feb05ca59950370ac5c214fb88f059734
eni1221 <yin8213@gmail.com>
2021-01-24 16:35:55 +0900

    arrayProblem

---
+++ .cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+
+
+	int n,a;
+	int max = -100;
+	int min = 1000;
+
+	scanf("%d", &n);// cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);// cin >> a;
+		if (a > max) {
+			max = a;
+		}
+		if (a < min) {
+			min = a;
+		}
+	}
+	//cout << min << " " << max << endl;
+	printf("%d %d", min, max);
+}
