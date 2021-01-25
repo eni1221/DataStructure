3c1400ca73574eeff6f5a1885f047df5400633e7
eni1221 <yin8213@gmail.com>
2021-01-25 10:02:17 +0900

    numberProblem

---
+++ array_number.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+int main() {
+
+	int a, b, c;
+	int sum;
+	int x;
+	int n[10]{ 0, };
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", &c);
+
+	sum = a * b * c;
+
+	for(int i=0;sum>0;i++) 
+	{
+		x = sum % 10;
+		n[x]++;
+		sum = sum / 10;
+
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d \n", n[i]);
+	}
+}
