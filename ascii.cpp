1228aa69087594af9ebf1fad84a85342aae1b681
eni1221 <yin8213@gmail.com>
2021-01-26 19:44:36 +0900

    asciiProblem

---
+++ ascii.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//int main()
+//{
+//	char c;
+//	scanf_s("%c", &c);
+//	printf("%d", c);
+//}
+
+#include <stdio.h>
+int main()
+{
+	int n, total = 0;
+	scanf("%d", &n);
+	char arr[1000];
+	scanf("%s", &arr);
+
+	for (int i = 0; i < n; i++)
+	{
+		total += arr[i] - '0';
+	}
+	
+	printf("%d", total);
+}
