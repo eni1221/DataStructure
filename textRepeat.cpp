37d426e37fe3feb2d17d442dee885b096bd3061c
eni1221 <yin8213@gmail.com>
2021-01-27 12:29:15 +0900

    testRepeatProblem

---
+++ textRepeat.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int main() {
+
+	char S[21];
+	char P[200];
+	int R, T;
+	int i, j;
+
+	scanf("%d", &T);
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %s", &R, S);
+
+		for (j = 0; j < 21; j++) {
+			for (int k = 0; k < R; k++) {
+				P[R * j + k] = S[j];
+			}
+		}
+		printf("%s\n", P);
+	}
+}
