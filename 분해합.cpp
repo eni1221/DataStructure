64a5c51965a642c0bb7a03127f1ec9c71e636bea
eni1221 <yin8213@gmail.com>
2021-02-08 15:48:34 +0900

    분해합

---
+++ 분해합.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int finder(void) {
+
+	int creator = 1;
+
+	while (1)
+	{
+		int copy = creator;
+		int sum = creator;
+
+		while (copy)
+		{
+			sum += copy % 10;
+			copy /= 10;
+		}
+
+		if (creator == N || sum == N)
+			break;
+
+		creator++;
+	}
+	return creator;
+}
+
+int main(void) 
+{
+	
+	cin >> N;
+
+	int result = finder();
+
+	if (result == N)
+		cout << result << endl;
+
+	else
+		cout << result << endl;
+	
+	return 0;
+
+}		
+
+
+
+
+
+
+
