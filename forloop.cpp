3e03fb85b32fb12d831406cfa95a5a979845ce2c
eni1221 <yin8213@gmail.com>
2021-02-25 20:30:16 +0900

    loopProblem

---
+++ forloop.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char b[50];
+    int x = 0, y = 0;
+
+    scanf("%s", b);
+
+    for (int i = 0; i <= 50; i++) {
+        if (b[i] == '(') {
+            x++;
+        }
+        else if (b[i] == ')') {
+            y++;
+        }
+
+    }
+    if (x == y) printf("YES");
+    else printf("NO");
+
+
+    return 0;
+}
