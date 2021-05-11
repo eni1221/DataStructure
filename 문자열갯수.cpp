095d60c0812afd89ed977c02d2ccaa73d7b8bed7
eni1221 <yin8213@gmail.com>
2021-05-11 23:11:15 +0900

    wordsum

---
+++ 문자열갯수.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+	char str[100001];
+	int spaceNum = 0;
+	int wordNum = 0;
+	int len;
+
+	scanf("%s", &str);
+	len = strlen(str);
+
+	for (int i = 0; i < len; i++) {
+		if (str[i] == ' ')
+			spaceNum++;
+	}
+	wordNum = spaceNum + 1;
+	if (isspace(str[0]))
+		wordNum--;
+
+	
+	return 0;
+}
