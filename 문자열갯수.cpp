31f1410191904aacd13d9eada25404e7ff192abe
eni1221 <yin8213@gmail.com>
2021-05-15 17:12:21 +0900

    wordsumQuestion

--- 문자열갯수.cpp
+++ 문자열갯수.cpp
@@ -2,26 +2,53 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <ctype.h>
 using namespace std;
 
+
 int main() {
-
-	char str[100001];
-	int spaceNum = 0;
-	int wordNum = 0;
-	int len;
-
-	scanf("%s", &str);
-	len = strlen(str);
-
-	for (int i = 0; i < len; i++) {
-		if (str[i] == ' ')
-			spaceNum++;
+	string s;
+	int num = 0;
+	int pos;
+	getline(cin, s);
+	if (!s.empty()) {
+		if (s.front() == ' ')
+			s.erase(s.begin());
+		if (!s.empty()) {
+			if (s.back() == ' ')
+				s.erase(s.end() - 1);
+			pos = s.find(' ');
+			while (pos != s.npos) {
+				num++;
+				pos = s.find(' ', pos + 1);
+			}
+			num++;
+		}
 	}
-	wordNum = spaceNum + 1;
-	if (isspace(str[0]))
-		wordNum--;
-
-	
+	cout << num << "\n";
 	return 0;
 }
+
+//int main() {
+//
+//	char str[100001];
+//	int spaceNum = 0;
+//	int wordNum = 0;
+//	int len;
+//
+//	gets_s(str);
+//	len = strlen(str);
+//
+//	for (int i = 0; i < len; i++) {
+//		if (str[i] == ' ')
+//			spaceNum++;
+//	}
+//	wordNum = spaceNum + 1;
+//	if (isspace(str[0]))
+//		wordNum--;
+//	if (isspace(str[len - 1]))
+//		wordNum--;
+//	printf("%d", wordNum);
+//	
+//	return 0;
+//}
