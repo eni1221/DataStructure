bd39e1b25d38bd21f72fd71db4604ccc24be168c
eni1221 <yin8213@gmail.com>
2021-05-18 16:53:56 +0900

    dialQuestion

---
+++ dial.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	vector <string> v1;
+	string dial;
+	//cin >> dial;
+	v1.push_back("ABC");
+	v1.push_back("DEF");
+	v1.push_back("GHI");
+	v1.push_back("JKL");
+	v1.push_back("MNO");
+	v1.push_back("PQRS");
+	v1.push_back("WXYZ");
+
+	cout << v1[0];
+	
+	
+}
