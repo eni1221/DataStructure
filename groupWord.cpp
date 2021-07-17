911e5af0b2fe889706c762b4007a8e2014b987c6
eni1221 <yin8213@gmail.com>
2021-07-17 14:35:42 +0900

    GroupWordQuestion

---
+++ groupWord.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+bool wordCheck(string str) {
+	bool alpabet[26] = { false };
+
+	for (int i = 0; i < str.length(); i++) {
+		if (alpabet[str[i] - 'a']) {
+			return false;
+		}
+		else {
+			char tmp = str[i];
+			alpabet[str[i] - 'a'] = true;
+
+			while (1) {
+				if (tmp != str[++i]) {
+					i--;
+					break;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	int count = 0;
+
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		if (wordCheck(str)) {
+			count++;
+		}
+	}
+	cout << count;
+
+	return 0;
+	
+}
