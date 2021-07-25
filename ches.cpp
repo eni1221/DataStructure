d1d8b3d4dbdf915bd634c4749bb74f08b003d919
eni1221 <yin8213@gmail.com>
2021-07-25 19:48:43 +0900

    ChesQuestion

---
+++ ches.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n, m;
+	cin >> n >> m;
+
+
+
+
+	return 0;
+}
