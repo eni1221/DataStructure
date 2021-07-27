b27de71677b03a42ea83fd1cb2f5597befa02e1c
eni1221 <yin8213@gmail.com>
2021-07-27 23:27:51 +0900

    chesQuetionsTwo:

--- ches.cpp
+++ ches.cpp
@@ -4,11 +4,66 @@
 #include <iostream>
 using namespace std;
 
+const int INF = 987654321;
+const int MAX = 50;
+
+int M, N;
+string board[MAX];
+
+string whiteFirst[8] = {
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"}
+};
+string blackFirst[8] = {
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"}
+};
+
+int whiteFirstChange(int y, int x) {
+	int cnt = 0;
+	for (int i = y; i < y + 8; i++)
+		for (int j = x; j < x + 8; j++)
+			if (board[i][j] != whiteFirst[i - y][j - x])
+				cnt++;
+	return cnt;
+}
+
+int blackFirstChange(int y, int x) {
+	int cnt = 0;
+	for (int i = y; i < y + 8; i++)
+		for (int j = x; j < x + 8; j++)
+			if (board[i][j] != blackFirst[i - y][j - x])
+				cnt++;
+	return cnt;
+}
+
+
+
 int main() {
 
-	int n, m;
-	cin >> n >> m;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> board[i];
+	}
+	int result = INF;
 
+	for (int i = 0; i + 7 < N; i++)
+		for (int j = 0; j + 7 < M; j++)
+			result = min(result, min(whiteFirstChange(i, j), blackFirstChange(i, j)));
+
+	cout << result <<endl;
 
 
 
