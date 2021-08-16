8b1605ffe6218891aa127db6a253e4958cb78f0e
eni1221 <yin8213@gmail.com>
2021-08-16 11:34:30 +0900

    brutePos

---
+++ DirectSom.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	int n;
+	cin >> n;
+	int i = 0;
+	string str = "666";
+	string sum = "666";
+	int num = 666;
+	while (i < n) {
+
+		string mm = to_string(num);
+		if(str.find("666")==mm)
+	}
+
+
+	return 0;
+}
