05d015d74fe4abf665833fb870d002d4f5ed901a
eni1221 <yin8213@gmail.com>
2021-09-01 15:48:01 +0900

    SortinsideProblem

---
+++ SortInside.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	string str;
+	cin >> str;
+	sort(str.begin(), str.end(), greater<char>());
+	cout << str;
+
+
+
+
+
+
+
+	return 0;
+}
