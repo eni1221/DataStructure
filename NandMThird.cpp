43c0e893bbc82929a8e7bd748c56a3fe487b8d44
eni1221 <yin8213@gmail.com>
2021-11-30 14:27:58 +0900

    NandMbruteMap

---
+++ NandMThird.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <utility>
+using namespace std;
+#define MAX 9
+
+int n, m;
+int arr[MAX] = { 0, };
+bool visited[MAX] = { 0, };
+
+void dfs( int cnt)
+{
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i =1; i <= n; i++) {
+		
+			visited[i] = true;
+			arr[cnt] = i;
+			dfs( cnt + 1);
+			visited[i] = false;
+	
+	}
+}
+
+
+int main() {
+
+
+	cin >> n >> m;
+	dfs(0);
+
+	return 0;
+}
