b3bcfa62861bf19aa136e088f76162d787a086da
eni1221 <yin8213@gmail.com>
2021-12-09 16:25:34 +0900

    StartandLink

---
+++ StartandLink.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <utility>
+using namespace std;
+
+
+
+int main() {
+
+
+
+	return 0;
+}
