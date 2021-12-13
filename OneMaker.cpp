2977387a496d074703b6cdf43ed8d17461c4610b
eni1221 <yin8213@gmail.com>
2021-12-13 21:52:49 +0900

    OneMaker

---
+++ OneMaker.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define MIN(x,y) (x<y?x:y)
+
+int dp[1000001];
+int main() {
+
+	int N;
+	scanf_s("%d", &N);
+
+	int i;
+	for (i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)
+		{
+			dp[i] = MIN(dp[i], dp[i / 2] + 1);
+		}
+		if (i % 3 == 0)
+		{
+			dp[i] = MIN(dp[i], dp[i / 3] + 1);
+		}
+	}
+	printf("%d\n", dp[N]);
+	return 0;
+}
+	
