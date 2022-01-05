a1e56da4d01e3a4be1ffe0117a3f24907eab4ae0
eni1221 <yin8213@gmail.com>
2022-01-05 23:25:35 +0900

    VeryLengthyIncreaseSubSequenceProblem

---
+++ VeryLengthyIncreaseSubSequence.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <math.h>
+#include <utility>
+#include<cstdio>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+
+	vector<int>arr(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int dp[1001];
+	fill_n(dp, 1001, 1);
+	int sol = 1;
+	for (int i = 0; i < n; ++i)
+	{
+		int code = arr[i];
+		for (int j = 0; j < i; ++j)
+		{
+			int value = arr[j];
+			if (code > value) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+
+		}
+		sol = max(sol, dp[i]);
+	}
+	cout << sol << "\n";
+	return 0;
+}
