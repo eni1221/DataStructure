6c593ee5c717a4ae82b07836d65aefe93886fc86
eni1221 <yin8213@gmail.com>
2022-01-17 15:17:44 +0900

    ContinuitySUm

---
+++ ContinuitySum.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <math.h>
+#include <utility>
+#include<cstdio>
+using namespace std;
+
+int main() {
+	int n, i;
+	int ans = 0;
+
+	scanf_s("%d", &n);
+	int arr[n];
+	int dp[n];
+
+	for (i = 0; i < n; i++)
+		scanf_s("%d", &arr[i]);
+
+	dp[0] = arr[0];
+	ans = dp[0];
+
+	for (i = 1; i < n; i++) {
+		dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+		ans = max(ans, dp[i]);
+	}
+	printf("%d", ans);
+	return 0;
+}
