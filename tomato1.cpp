d1a08cca082292eee340efaae15401b38f38759c
eni1221 <yin8213@gmail.com>
2022-01-23 23:57:22 +0900

    TOMATOQUESTION

---
+++ tomato1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <math.h>
+#include <utility>
+#include<cstdio>
+using namespace std;
+#include <queue>
+
+int a[1001][1001];
+int dx[] = { 0,-1,0,1 };
+int dy[] = { -1,0,1,0 };
+int d[1001][1001];
+
+int main() {
+	int m, n;
+	cin > m >> n;
+	queue<pair<int, int>>q;
+	for (int i = 0; i < n; i++)
+	{
+
+	}
+}
